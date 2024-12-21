b1baa900e21a60e81cbdfc436812f015697f840b
Nick Dodonov <nick.dodonov@gmail.com>
2024-12-21 19:00:05 +0100

    client - allow to select hosting server url for http request tests; prepare to test HttpClient in WebGL

--- client/Assets/Scripts/HudLogic.cs
+++ client/Assets/Scripts/HudLogic.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using Diagnostics;
+using Shared;
 using Shared.Meta.Client;
 using TMPro;
 using UnityEngine;
@@ -13,20 +16,89 @@ public class HudLogic : MonoBehaviour
     public Button serverRequestButton;
     public TMP_Text serverResponseText;
 
+    private record ServerOption(string Text, string Url);
+    private readonly List<ServerOption> _serverOptions = new();
+
     private void OnEnable()
     {
         Shared.StaticLog.Info("==== starting client ====");
         StartupInfo.Print();
         versionText.text = $"Version: {Application.version}";
         
+        if (NeedServerLocalhostOptions())
+        {
+            _serverOptions.Add(new("localhost-debug", "http://localhost:5270"));
+            _serverOptions.Add(new("localhost-compose", "https://localhost"));
+        }
+        if (NeedServerHostingOption(out var url))
+            _serverOptions.Add(new("hosting", url));
         serverDropdown.options.Clear();
-#if UNITY_EDITOR
-        serverDropdown.options.Add(new TMP_Dropdown.OptionData("http://localhost:5270"));
-#endif
-        serverDropdown.options.Add(new TMP_Dropdown.OptionData("https://TODO-BuildOption"));
+        serverDropdown.options.AddRange(
+            _serverOptions.Select(x => new TMP_Dropdown.OptionData(x.Text)));
         serverRequestButton.onClick.AddListener(OnServerRequestButtonClick);
         serverResponseText.text = "";
     }
+
+    private static bool NeedServerLocalhostOptions()
+    {
+        var absoluteURL = Application.absoluteURL;
+        if (string.IsNullOrEmpty(absoluteURL))
+            return true; // running in editor
+        if (absoluteURL.Contains("localhost"))
+            return true;
+        return false;
+    }
+
+    private bool NeedServerHostingOption(out string url)
+    {
+        url = Application.absoluteURL;
+        if (!string.IsNullOrEmpty(url))
+            url = new Uri(url).GetLeftPart(UriPartial.Authority);
+
+#if UNITY_EDITOR
+        if (NeedServerLocalhostOptions())
+        {
+            var env = ParseEnvFileToDictionary();
+            if (env.TryGetValue("SERVER_URL", out url))
+                StaticLog.Info($"server url: {url}");
+        }
+
+        static Dictionary<string, string> ParseEnvFileToDictionary()
+        {
+            var directory = System.IO.Directory.GetCurrentDirectory();
+            Dictionary<string, string> envVariables = new();
+
+            while (!string.IsNullOrEmpty(directory))
+            {
+                var envFilePath = System.IO.Path.Combine(directory, ".env");
+                if (System.IO.File.Exists(envFilePath))
+                {
+                    var lines = System.IO.File.ReadAllLines(envFilePath);
+                    foreach (var line in lines)
+                    {
+                        var trimmedLine = line.Trim();
+                        if (string.IsNullOrEmpty(trimmedLine) || trimmedLine.StartsWith("#"))
+                            continue;
+
+                        var splitIndex = trimmedLine.IndexOf('=');
+                        if (splitIndex <= 0)
+                            continue;
+                    
+                        var key = trimmedLine[..splitIndex].Trim();
+                        var value = trimmedLine[(splitIndex + 1)..].Trim();
+                        envVariables[key] = value;
+                    }
+                    break; // Stop searching once the .env file is found and processed
+                }
+                directory = System.IO.Path.GetDirectoryName(directory); // Move up one directory
+            }
+
+            return envVariables;
+        }
+#endif
+        return true;
+    }
+
 
     private void OnDisable()
     {
@@ -38,9 +110,8 @@ public class HudLogic : MonoBehaviour
         try
         {
             serverResponseText.text = "Requesting server...";
-            var selectedServer = serverDropdown.options[serverDropdown.value].text;
-            var serverAddress = new Uri(selectedServer);
-            var client = new HttpClient { BaseAddress = serverAddress };
+            var url = _serverOptions[serverDropdown.value].Url;
+            var client = new HttpClient { BaseAddress = new(url) };
             var meta = new MetaClient(client);
             var result = await meta.GetInfo(destroyCancellationToken);
             serverResponseText.text = $"Server Response: {result.RequestTime}";
---
+++ shared/System.Runtime/CompilerServices.cs
@@ -0,0 +1,18 @@
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// https://docs.unity3d.com/Manual/CSharpCompiler.html
+    /// 
+    /// C# 9 init and record support comes with a few caveats.
+    /// * The type System.Runtime.CompilerServices.IsExternalInit is required for full record support
+    ///   as it uses init only setters, but is only available in .NET 5 and later (which Unity doesn’t support).
+    ///   Users can work around this issue by declaring the System.Runtime.CompilerServices.IsExternalInit type
+    ///   in their own projects.
+    /// * You shouldn’t use C# records in serialized types because Unity’s serialization system doesn’t support C# records.
+    /// 
+    /// </summary>
+    public static class IsExternalInit
+    {
+    }
+}
