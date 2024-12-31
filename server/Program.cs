b263c7138d565439467c11f696c31b953c5be453
Nick Dodonov <nick.dodonov@gmail.com>
2024-12-31 10:59:27 +0100

    server - add cors for local execution (simplify develop)

--- server/Program.cs
+++ server/Program.cs
@@ -23,11 +23,25 @@ builder.Services
         o.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
 
+// Adding CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() // Allow all origins
+            .AllowAnyMethod() // Allow all HTTP methods (GET, POST, etc.)
+            .AllowAnyHeader(); // Allow all headers
+    });
+});
+
 var app = builder.Build();
-app.Logger.LogInformation("Starting server");
+app.Logger.LogInformation("Starting server (TODO: build version)");
 
 // Configure the HTTP request pipeline.
 app.UseAuthorization();
+// Apply CORS middleware
+app.UseCors();
+
 app.MapControllers();
 
 app.Run();
