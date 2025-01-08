0f89163414683c8df1191540865bf9c4a5dbe5c1
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-08 17:37:16 +0100

    lgc - prepare stub player input handler with UI display

---
+++ src/Client/Assets/Scripts/PlayerTap.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerTap : MonoBehaviour
+{
+    private InputAction _clickAction;
+    private InputAction _pointAction;
+
+    private void Start()
+    {
+        _clickAction = InputSystem.actions.FindAction("UI/Click");
+        _pointAction = InputSystem.actions.FindAction("UI/Point");
+    }
+
+    private void Update()
+    {
+        if (_clickAction.IsPressed())
+        {
+            var pointValue = _pointAction.ReadValue<Vector2>();
+            //var cam = Camera.main;
+            if (Screen.safeArea.Contains(pointValue)) // && cam != null)
+            {
+                //Shared.Log.Slog.Info($"{pointValue.x}, {pointValue.y}");
+                //var ray = cam.ScreenPointToRay(pointValue);
+                transform.position = pointValue;
+            }
+        }
+    }
+}
