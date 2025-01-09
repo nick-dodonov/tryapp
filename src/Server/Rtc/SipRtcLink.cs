9aaa753a542b0da6b4b90e001ce9e77362c624e4
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-09 19:30:52 +0100

    rtc - sip - mv ice processing init

--- src/Server/Rtc/SipRtcLink.cs
+++ src/Server/Rtc/SipRtcLink.cs
@@ -9,7 +9,7 @@ internal class SipRtcLink(string id, RTCPeerConnection peerConnection, ILoggerFa
     private readonly ILogger _logger = new PeerIdLogger(loggerFactory.CreateLogger<SipRtcLink>(), id);
 
     public readonly RTCPeerConnection PeerConnection = peerConnection;
-    public readonly List<RTCIceCandidate> IceCandidates = [];
+    private readonly List<RTCIceCandidate> _iceCandidates = [];
     public readonly TaskCompletionSource<List<RTCIceCandidate>> IceCollectCompleteTcs = new();
     public RTCDataChannel? DataChannel;
     public ClientState LastClientState;
@@ -22,6 +22,27 @@ internal class SipRtcLink(string id, RTCPeerConnection peerConnection, ILoggerFa
             ordered = false,
             maxRetransmits = 0
         });
+        
+        PeerConnection.onicecandidate += candidate =>
+        {
+            _logger.LogDebug($"onicecandidate: {candidate}");
+            // if (candidate.type == RTCIceCandidateType.host)
+            // {
+            //     _logger.LogDebug("onicecandidate: skip host candidate");
+            //     return;
+            // }
+            _iceCandidates.Add(candidate);
+        };
+        PeerConnection.onicecandidateerror += (candidate, error) =>
+            _logger.LogWarning($"onicecandidateerror: '{error}' {candidate}");
+        PeerConnection.oniceconnectionstatechange += state =>
+            _logger.LogDebug($"oniceconnectionstatechange: {state}");
+        PeerConnection.onicegatheringstatechange += state =>
+        {
+            _logger.LogDebug($"onicegatheringstatechange: {state}");
+            if (state == RTCIceGatheringState.complete)
+                IceCollectCompleteTcs.SetResult(_iceCandidates);
+        };
     }
 
     private class PeerIdLogger(ILogger inner, string peerId) : ILogger
