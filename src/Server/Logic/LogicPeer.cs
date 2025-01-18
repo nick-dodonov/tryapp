fa8f585937acfbcff22c1d129f05d60f3de67bb0
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-18 21:37:17 +0100

    logic - rfx peer send

--- src/Server/Logic/LogicPeer.cs
+++ src/Server/Logic/LogicPeer.cs
@@ -5,37 +5,27 @@ using Shared.Web;
 
 namespace Server.Logic;
 
-public class LogicPeer : IDisposable
+public sealed class LogicPeer : IDisposable
 {
+    private readonly ILogger _logger;
+    private readonly LogicSession _session;
     private readonly ITpLink _link;
+
     private readonly System.Timers.Timer _timer;
-    private int _frameId;
 
     public ClientState LastClientState { get; set; }
 
     public LogicPeer(ILogger logger, LogicSession session, ITpLink link)
     {
+        _logger = logger;
+        _session = session;
         _link = link;
         _timer = new(1000);
+
+        var frame = 0;
         _timer.Elapsed += (_, _) =>
         {
-            var utcMs = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-            //var msg = $"{frameId};TODO-FROM-SERVER;{utcMs}";
-
-            var peerStates = session.GetPeerStates();
-            var serverStateMsg = new ServerState
-            {
-                Frame = _frameId,
-                UtcMs = utcMs,
-                Peers = peerStates
-            };
-            _frameId++;
-
-            var msg = WebSerializer.SerializeObject(serverStateMsg);
-            logger.Info($"Send: {msg}");
-            
-            var bytes = System.Text.Encoding.UTF8.GetBytes(msg);
-            link.Send(bytes);
+            Send(frame++);
         };
         _timer.Start();
     }
@@ -44,5 +34,23 @@ public class LogicPeer : IDisposable
     {
         _timer.Dispose();
         _link.Dispose();
+    }
+
+    private void Send(int frame)
+    {
+        var utcMs = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        var peerStates = _session.GetPeerStates();
+        var serverStateMsg = new ServerState
+        {
+            Frame = frame,
+            UtcMs = utcMs,
+            Peers = peerStates
+        };
+
+        var msg = WebSerializer.SerializeObject(serverStateMsg);
+        _logger.Info(msg);
+
+        var bytes = System.Text.Encoding.UTF8.GetBytes(msg);
+        _link.Send(bytes);
     }
 }
