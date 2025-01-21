7cf3a5ede3a0a742c88d9fc5c68b1b40f86b434c
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-21 23:00:40 +0100

    tp - dump - add logging for buffer end too

--- src/Shared/Runtime/Tp/Ext/Misc/DumpLink.cs
+++ src/Shared/Runtime/Tp/Ext/Misc/DumpLink.cs
@@ -11,7 +11,8 @@ namespace Shared.Tp.Ext.Misc
     //  possibly introduce separate LayerLink with custom processors list
     public class DumpLink : ExtLink
     {
-        private const int MaxLogBytes = 100;
+        private const int StartBytes = 100;
+        private const int EndBytes = 20;
 
         private readonly ILogger _logger = null!;
         private static readonly UTF8Encoding _utf8Encoding = new(false, false);
@@ -50,23 +51,39 @@ namespace Shared.Tp.Ext.Misc
             base.Received(link, span);
         }
 
+        private static readonly char[] _midEllipsis = { ' ', '…', ' ' }; // '⋯' '…' Unicode Ellipsis
         private static void Log(ILogger logger, ReadOnlySpan<byte> span, string prefix, string member)
         {
-            var maxBytesToConvert = Math.Min(span.Length, MaxLogBytes);
-            Span<char> chars = stackalloc char[maxBytesToConvert];
-
-            var charsWritten = _utf8Encoding.GetChars(span[..maxBytesToConvert], chars);
-            for (var i = 0; i < charsWritten; i++)
+            const int maxBytes = StartBytes + EndBytes;
+            Span<char> chars = stackalloc char[maxBytes + _midEllipsis.Length];
+            int written;
+            if (span.Length <= maxBytes)
             {
-                if (char.IsControl(chars[i]) || chars[i] == '\uFFFD')
-                    chars[i] = '\u00b7'; // '·' Unicode Replacement Character or control check
+                written = Convert(span, chars);
+            }
+            else
+            {
+                written = Convert(span[..StartBytes], chars);
+                _midEllipsis.CopyTo(chars[written..]);
+                written += _midEllipsis.Length;
+                written += Convert(span[^EndBytes..], chars[written..]);
             }
 
-            var ellipsis = maxBytesToConvert < span.Length ? "…" : null; // '⋯' '…' Unicode Ellipsis
-            var charsStr = new string(chars[..charsWritten]);
-            logger.Info(
-                $"{prefix}: [{span.Length}] bytes: {charsStr}{ellipsis}", //｟｠⦅⦆ «»
-                member: member);
+            var charsStr = new string(chars[..written]);
+            logger.Info($"{prefix}: [{span.Length}] bytes: {charsStr}", member: member); //｟｠⦅⦆ «»
+            return;
+
+            static int Convert(ReadOnlySpan<byte> span, Span<char> chars)
+            {
+                var charsWritten = _utf8Encoding.GetChars(span, chars);
+                for (var i = 0; i < charsWritten; i++)
+                {
+                    if (char.IsControl(chars[i]) || chars[i] == '\uFFFD')
+                        chars[i] = '\u00b7'; // '·' Unicode Replacement Character or control check
+                }
+
+                return charsWritten;
+            }
         }
     }
 }
