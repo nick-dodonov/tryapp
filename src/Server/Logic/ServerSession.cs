8eb749de106b781078dceb55bc84abc7eb1549e9
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-23 00:50:29 +0100

    srv - session updates are switched on/off depending on connected at least one peer - it saves CPU on stage server now (don't want to waste it on idle running server)

--- src/Server/Logic/ServerSession.cs
+++ src/Server/Logic/ServerSession.cs
@@ -9,12 +9,23 @@ using Shared.Tp.Ext.Misc;
 
 namespace Server.Logic;
 
-public class ServerSession(ILoggerFactory loggerFactory, ITpApi tpApi, ILogicLooper logicLooper)
+public class ServerSession(ILoggerFactory loggerFactory, ITpApi tpApi)
     : IHostedService, ITpListener, ITpReceiver
 {
     private readonly ILogger _logger = loggerFactory.CreateLogger<ServerSession>();
 
     private readonly TimeLink.Api _timeApi = tpApi.Find<TimeLink.Api>() ?? throw new("TimeLink.Api not found");
+
+    /// <summary>
+    /// Session updates are switched on/off depending on connected at least one peer.
+    /// It saves CPU on stage server now (don't want to waste it on idle running server).
+    /// about looper: https://github.com/Cysharp/LogicLooper?tab=readme-ov-file#usage
+    /// TODO: mv on/off to DynamicLooper (it will also allows to change target frame rate on the fly)
+    /// </summary>
+    private ILogicLooper? _logicLooper;
+    private Task? _looperShutdownTask;
+
+    private int _peerCount;
     private readonly ConcurrentDictionary<ITpLink, ServerPeer> _peers = new();
 
     private readonly IVirtualPeer[] _virtualPeers =
@@ -28,32 +39,18 @@ public class ServerSession(ILoggerFactory loggerFactory, ITpApi tpApi, ILogicLoo
     {
         _logger.Info("start listening");
         tpApi.Listen(this);
-
-        // https://github.com/Cysharp/LogicLooper/blob/master/samples/LoopHostingApp/LoopHostedService.cs
-        _ = logicLooper.RegisterActionAsync(Update, cancellationToken);
         return Task.CompletedTask;
     }
 
     async Task IHostedService.StopAsync(CancellationToken cancellationToken)
     {
-        await logicLooper.ShutdownAsync(TimeSpan.FromSeconds(5));
-        var remainedActions = logicLooper.ApproximatelyRunningActions;
-        if (remainedActions > 0)
-            _logger.Warn($"{remainedActions} actions are remained in loop");
-    }
-
-    private bool Update(in LogicLooperActionContext ctx, CancellationToken state)
-    {
-        if (ctx.CancellationToken.IsCancellationRequested)
+        StopUpdates();
+        var lastShutdownTask = _looperShutdownTask;
+        if (lastShutdownTask != null)
         {
-            _logger.Info("shutdown");
-            return false;
+            _logger.Info("awaiting last looper shutdown");
+            await lastShutdownTask;
         }
-
-        //_logger.Info($"{ctx.CurrentFrame}");
-        foreach (var virtualPeer in _virtualPeers)
-            virtualPeer.Update((float)ctx.ElapsedTimeFromPreviousFrame.TotalSeconds);
-        return true;
     }
 
     ITpReceiver ITpListener.Connected(ITpLink link)
@@ -63,6 +60,8 @@ public class ServerSession(ILoggerFactory loggerFactory, ITpApi tpApi, ILogicLoo
             new IdLogger(loggerFactory.CreateLogger<ServerPeer>(), link.GetRemotePeerId()),
             this, link);
         _peers.TryAdd(link, peer);
+        if (Interlocked.Increment(ref _peerCount) == 1)
+            StartUpdates();
         return this;
     }
 
@@ -80,11 +79,77 @@ public class ServerSession(ILoggerFactory loggerFactory, ITpApi tpApi, ILogicLoo
         {
             _logger.Info($"peer disconnected: {link}");
             peer.Dispose();
+            if (Interlocked.Decrement(ref _peerCount) == 0)
+                StopUpdates();
         }
         else
             _logger.Warn($"peer not found: {link}");
     }
 
+    private async void StartUpdates()
+    {
+        try
+        {
+            var previousShutdownTask = _looperShutdownTask;
+            if (previousShutdownTask != null)
+            {
+                _logger.Info("awaiting previous looper shutdown");
+                await previousShutdownTask;
+            }
+
+            //TODO: appsettings.json option (or possibly control from client for now)
+            const int targetFrameRate = 10;
+            _logger.Info($"starting loop: targetFrameRate={targetFrameRate}");
+            _logicLooper = new LogicLooper(targetFrameRate);
+            _ = _logicLooper.RegisterActionAsync(Update);
+        }
+        catch (Exception e)
+        {
+            _logger.Error($"{e}");
+        }
+    }
+
+    private async void StopUpdates()
+    {
+        try
+        {
+            var logicLooper = Interlocked.Exchange(ref _logicLooper, null);
+            if (logicLooper == null)
+                return;
+            var previousShutdownTask = _looperShutdownTask;
+            if (previousShutdownTask != null)
+            {
+                _logger.Info("awaiting previous looper shutdown");
+                await previousShutdownTask;
+            }
+            _logger.Info($"actions in loop: {logicLooper.ApproximatelyRunningActions}");
+            _looperShutdownTask = logicLooper.ShutdownAsync(TimeSpan.FromSeconds(1));
+            await _looperShutdownTask;
+            _looperShutdownTask = null;
+            var remainedActions = logicLooper.ApproximatelyRunningActions;
+            if (remainedActions > 0)
+                _logger.Warn($"still running actions in loop: {remainedActions}");
+        }
+        catch (Exception e)
+        {
+            _logger.Error($"{e}");
+        }
+    }
+
+    private bool Update(in LogicLooperActionContext ctx)
+    {
+        if (ctx.CancellationToken.IsCancellationRequested)
+        {
+            _logger.Info("shutdown");
+            return false;
+        }
+
+        //_logger.Info($"{ctx.CurrentFrame}");
+        foreach (var virtualPeer in _virtualPeers)
+            virtualPeer.Update((float)ctx.ElapsedTimeFromPreviousFrame.TotalSeconds);
+        return true;
+    }
+    
     public ServerState GetServerState(int frame)
     {
         var sessionMs = _timeApi.LocalMs;
