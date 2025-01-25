9ba0dd55bcb9f08eda75b51c3047d3c2d4ad35bb
Nick Dodonov <nick.dodonov@gmail.com>
2025-01-25 20:48:36 +0100

    client - fix incorrect read-only for dump stats

--- src/Client/Assets/Scripts/Logic/ClientOptionsEditor.cs
+++ src/Client/Assets/Scripts/Logic/ClientOptionsEditor.cs
@@ -1,6 +1,5 @@
 using RuntimeInspectorNamespace;
 using Shared.Tp.Ext.Misc;
-using UnityEngine;
 using UnityEngine.Scripting;
 using UnityEngine.UI;
 
@@ -9,15 +8,14 @@ namespace Client.Logic
     /// <summary>
     /// Sample of readonly runtime editor for specific type
     /// </summary>
-    [Preserve, RuntimeInspectorCustomEditor(typeof(DumpLink.StatsInfo), false)]
+    [Preserve, RuntimeInspectorCustomEditor(typeof(DumpLink.StatsInfo))]
     public class ColliderEditor : IRuntimeInspectorCustomEditor
     {
         public void GenerateElements(ObjectField parent)
         {
             parent.CreateDrawersForVariables(null);
 
-            var inputFields = Object.FindObjectsByType<InputField>(
-                FindObjectsInactive.Include, FindObjectsSortMode.None);
+            var inputFields = parent.gameObject.GetComponentsInChildren<InputField>(true);
             foreach (var inputField in inputFields)
                 inputField.interactable = false;
         }
