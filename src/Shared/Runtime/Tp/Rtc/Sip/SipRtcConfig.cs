c54ea939220972e647880338d39c469a1f31bde1
Nick Dodonov <nick.dodonov@gmail.com>
2025-03-24 22:03:40 +0100

    client - prepare for client ice servers setup - introduce shared with server rtc config

--- src/Shared/Runtime/Tp/Rtc/Sip/SipRtcConfig.cs
+++ src/Shared/Runtime/Tp/Rtc/Sip/SipRtcConfig.cs
@@ -1,19 +1,10 @@
 using System;
-using System.Collections.Generic;
 
 namespace Shared.Tp.Rtc.Sip
 {
     [Serializable]
-    public class SipRtcConfig
+    public class SipRtcConfig : RtcConfig
     {
-        public List<RtcIceServer>? IceServers { get; set; }
-    }
-
-    [Serializable]
-    public class RtcIceServer
-    {
-        public string? Url { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
+        //TODO: SIPSorcery.Net.RTCPeerConnection port range settings here
     }
 }
