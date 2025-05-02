416e247726a1590c8509742e1b775fffb062ebf6
Nick Dodonov <nick.dodonov@gmail.com>
2025-05-02 13:07:51 +0200

    app - peer displays its history; prepare for custom interpolation

--- src/App/Client/Assets/Scripts/Logic/PeerView.cs
+++ src/App/Client/Assets/Scripts/Logic/PeerView.cs
@@ -18,38 +18,48 @@ namespace Client.Logic
         public bool Changed => _changed;
         public void SetChanged(bool changed) => _changed = changed;
 
+        private PeerState _peerState;
+
         private float _applySessionMs;
         private Color _applyColor;
 
-        public void ApplyState(in PeerState peerState, History<ServerState> serverHistory)
+        public void ApplyState(in PeerState peerState, History<ServerState> history)
         {
-            var state = peerState.ClientState;
-            _applySessionMs = state.Ms;
+            _peerState = peerState;
+            ApplyHistory(history);
 
-            lineRenderer.positionCount = 2;
-            lineRenderer.SetPosition(0, transform.position);
-            transform.position = state.LoadPosition();
-            lineRenderer.SetPosition(1, transform.position);
+            ref var clientState = ref _peerState.ClientState;
+            transform.position = clientState.LoadPosition();
 
             // Convert back from uint to Color32 and assign it to image.color
-            var colorU = state.Color;
+            var colorU = clientState.Color;
             var color32 = new Color32(
                 (byte)((colorU >> 16) & 0xFF),
                 (byte)((colorU >> 8) & 0xFF),
                 (byte)(colorU & 0xFF),
                 0xFF);
             _applyColor = color32;
-
+            _applySessionMs = clientState.Ms;
             ApplyColor();
 
             idText.text = peerState.Id;
 
             _changed = true;
+        }
 
-            //TODO: history diagnostics
-            // {
-            //     lineRenderer.positionCount = serverHistory.Count;
-            // }
+        private void ApplyHistory(History<ServerState> history)
+        {
+            var index = 0;
+            lineRenderer.positionCount = 0;
+            foreach (ref var serverState in history.ReverseRefValues)
+            {
+                if (!serverState.TryGetPeerStateIndex(_peerState.Id, out var peerIndex))
+                    continue;
+
+                ref var peerState = ref serverState.Peers[peerIndex];
+                lineRenderer.positionCount = index + 1;
+                lineRenderer.SetPosition(index++, peerState.ClientState.LoadPosition());
+            }
         }
 
         private const float FadeAlphaMin = 0.1f;
@@ -60,7 +70,6 @@ namespace Client.Logic
             var t = (sessionMs - _applySessionMs) / FadeAlphaSec / 1000.0f;
             var alpha = Mathf.Lerp(1, FadeAlphaMin, t);
             _applyColor.a = alpha;
-
             ApplyColor();
         }
 
@@ -68,6 +77,30 @@ namespace Client.Logic
         {
             image.color = _applyColor;
             meshRenderer.material.color = _applyColor;
+        }
+    }
+
+    public static class ServerStateExtensions
+    {
+        //TODO: cache Peers by Id in ServerState
+        public static bool TryGetPeerStateIndex(this in ServerState serverState, string peerId, out int peerIndex)
+        {
+            peerIndex = -1;
+
+            var peers = serverState.Peers;
+            if (peers == null)
+                return false;
+
+            for (var i = 0; i < peers.Length; ++i)
+            {
+                if (peers[i].Id != peerId)
+                    continue;
+
+                peerIndex = i;
+                return true;
+            }
+
+            return false;
         }
     }
 }
