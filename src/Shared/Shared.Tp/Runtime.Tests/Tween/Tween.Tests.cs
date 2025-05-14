fd22ab1e6cec3dca262152a5b9f51aebae229f81
Nick Dodonov <nick.dodonov@gmail.com>
2025-05-14 22:49:40 +0200

    tween tests - prepare stub for next impls

--- src/Shared/Shared.Tp/Runtime.Tests/Tween/Tween.Tests.cs
+++ src/Shared/Shared.Tp/Runtime.Tests/Tween/Tween.Tests.cs
@@ -1,13 +1,45 @@
 using NUnit.Framework;
+using UnityEngine.TestTools.Constraints;
+using Is = UnityEngine.TestTools.Constraints.Is;
 
 namespace Shared.Tp.Tests.Tween
 {
+    public struct TestUnmanaged
+    {
+        public int I;
+        public float F;
+
+        public static TestUnmanaged Make(int idx)
+        {
+            return new()
+            {
+                I = 11 * (idx + 1),
+                F = 11.1f * (idx + 1),
+            };
+        }
+
+        public static void CustomTween(ref TestUnmanaged v0, ref TestUnmanaged v1, float t, ref TestUnmanaged vr)
+        {
+            vr.I = (int)(v0.I * (1 - t) + v1.I * t);
+            vr.F = v0.F * (1 - t) + v1.F * t;
+        }
+    }
+
     public class TweenTests
     {
         [Test]
-        public void TweenSimpleTest()
+        public void CustomTweenUnmanaged_NoAllocations()
         {
-            Assert.Pass();
+            var v0 = TestUnmanaged.Make(0);
+            var v1 = TestUnmanaged.Make(1);
+            var vr = TestUnmanaged.Make(1);
+
+            Assert.That(() => {
+                TestUnmanaged.CustomTween(ref v0, ref v1, 0.5f, ref vr);
+            }, Is.Not.AllocatingGCMemory());
+
+            Assert.That(vr.I, Is.InRange(v0.I, v1.I));
+            Assert.That(vr.F, Is.InRange(v0.F, v1.F));
         }
     }
 }
