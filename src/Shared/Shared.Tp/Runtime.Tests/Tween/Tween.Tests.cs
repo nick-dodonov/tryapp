f73c0191eae42326e4b05deb75a0a0711e849af4
Nick Dodonov <nick.dodonov@gmail.com>
2025-05-14 08:03:00 +0100

    cl - stub for next generic solution

---
+++ src/Shared/Shared.Tp/Runtime.Tests/Tween/Tween.Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Shared.Tp.Tests.Tween
+{
+    public class TweenTests
+    {
+        [Test]
+        public void TweenSimpleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
