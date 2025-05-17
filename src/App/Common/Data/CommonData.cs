cef2a3aea696e4cd7dd481b24241538825872901
Nick Dodonov <nick.dodonov@gmail.com>
2025-05-17 13:25:48 +0200

    common - add tests shared assembly to speedup app-specific checks

--- src/App/Common/Data/CommonData.cs
+++ src/App/Common/Data/CommonData.cs
@@ -1,5 +1,4 @@
 using MemoryPack;
-using Shared.Tp.Data.Mem;
 using Shared.Tp.Data.Mem.Formatters;
 
 namespace Common.Data
---
+++ src/App/Common/Tests/Data.Tween.Tests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Common.Tests
+{
+    public class DataTweenTests
+    {
+        [Test]
+        public void Simple()
+        {
+        }
+    }
+}
