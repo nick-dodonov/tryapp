ba556df910793de02288ee636d1fed591a59371b
Nick Dodonov <nick.dodonov@gmail.com>
2025-05-25 20:30:34 +0200

    time - prepare to optimize and smooth calculations

--- src/Shared/Shared.Tp/Runtime/Ext/Misc/TimeLink.cs
+++ src/Shared/Shared.Tp/Runtime/Ext/Misc/TimeLink.cs
@@ -6,6 +6,15 @@ using Shared.Tp.Util;
 
 namespace Shared.Tp.Ext.Misc
 {
+    // Enough to keep 4 seconds in 60 fps sends.
+    //  In case of long lag (>4 sec) rtt calculation will be wrong.
+    //  However, we don't need more because history to restore the state cannot keep more than 1 second of states,
+    //  and in the case rtt exceeds, we need to re-initialize the state.
+    // 
+    // Anyway (in the case of a requirement of 1000 fps sending),
+    //  we can change this type either and/or its serialize/deserialize technique. 
+    using TimeLinkLocalTicksIndex = Byte; 
+
     /// <summary>
     /// TODO: add api for client/server session to obtain "session time"
     /// TODO: make average and deviation RTT calculations, throw anything with 3-sigma rule
@@ -50,9 +59,11 @@ namespace Shared.Tp.Ext.Misc
                 new(_startTicks) { InnerLink = innerLink };
         }
 
-        private readonly long _startTicks; //rt
+        private readonly long _startTicks;
 
+        private TimeLinkLocalTicksIndex _receivedRemoteIdx;
         private long _receivedRemote; //rt
+
         private long _receivedLocal; //rt
 
         private int _rtt; //rt
@@ -99,20 +110,44 @@ namespace Shared.Tp.Ext.Misc
             base.Received(link, span);
         }
 
+        private unsafe struct Details {
+            private TimeLinkLocalTicksIndex _historyIndex;
+            private fixed long _localTicksHistory[TimeLinkLocalTicksIndex.MaxValue + 1];
+
+            public TimeLinkLocalTicksIndex AddLocalTicks(long localTicks)
+            {
+                var historyIndex = _historyIndex++;
+                _localTicksHistory[historyIndex] = localTicks;
+                return historyIndex;
+            }
+
+            public long GetLocalTicks(TimeLinkLocalTicksIndex historyIndex)
+            {
+                return _localTicksHistory[historyIndex];
+            }
+        }
+
+        private Details _details;
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void WriteTime(IBufferWriter<byte> writer)
         {
             var local = LocalRt;
+            var localIdx = _details.AddLocalTicks(local);
 
-            var passedLocal = _receivedLocal != 0 ? local - _receivedLocal : 0;
+            writer.Write(localIdx);
             writer.Write(local);
 
             //adjustment from previous reception, so the remote side can correctly calculate rtt
+            var passedLocal = _receivedLocal != 0 ? local - _receivedLocal : 0;
             var receivedRemote = _receivedRemote;
             receivedRemote += passedLocal;
+            writer.Write(_receivedRemoteIdx);
             writer.Write(receivedRemote);
+            var receivedSendingDelta = (short)(local - _receivedLocal);
+            writer.Write(receivedSendingDelta);
 
-            //Slog.Info($"local={local} remoteAdjusted={receivedRemote} (passed={passedLocal})");
+            //Slog.Info($"localIdx={localIdx:000} local={local} remoteAdjusted={receivedRemote} (passed={passedLocal})");
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -120,18 +155,37 @@ namespace Shared.Tp.Ext.Misc
         {
             var local = LocalRt;
 
-            const int length = sizeof(long) * 2;
+            const int length = 
+                sizeof(TimeLinkLocalTicksIndex) + sizeof(long) +
+                sizeof(TimeLinkLocalTicksIndex) + sizeof(long) +
+                sizeof(short);
             var timeSpan = span[^length..];
-            fixed (byte* ptr = timeSpan)
+            fixed (byte* ptrStart = timeSpan)
             {
+                var ptr = ptrStart;
+                _receivedRemoteIdx = Unsafe.ReadUnaligned<TimeLinkLocalTicksIndex>(ptr);
+                ptr += sizeof(TimeLinkLocalTicksIndex);
+
                 _receivedLocal = local;
                 _receivedRemote = Unsafe.ReadUnaligned<long>(ptr);
-                var sentLocalAdjusted = Unsafe.ReadUnaligned<long>(ptr + sizeof(long));
+                ptr += sizeof(long);
+
+                var sentLocalIdx = Unsafe.ReadUnaligned<TimeLinkLocalTicksIndex>(ptr);
+                ptr += sizeof(TimeLinkLocalTicksIndex);
+                var sentLocal = _details.GetLocalTicks(sentLocalIdx);
+                var rtt2 = (int)(local - sentLocal);
+
+                var sentLocalAdjusted = Unsafe.ReadUnaligned<long>(ptr);
+                ptr += sizeof(long);
+
+                var receivedSendingDelta = Unsafe.ReadUnaligned<short>(ptr);
+                //ptr += sizeof(short);
+                rtt2 -= receivedSendingDelta;
 
                 if (sentLocalAdjusted != 0)
                     _rtt = (int)(local - sentLocalAdjusted);
 
-                //Slog.Info($"local={local} remote={_receivedRemote} sentLocalAdjusted={sentLocalAdjusted} (rtt={_rtt})");
+                Slog.Info($"remoteIdx={_receivedRemoteIdx:000} local={local} remote={_receivedRemote} sentLocalAdjusted={sentLocalAdjusted} rtt={_rtt} rtt2={rtt2}");
             }
 
             return span[..^length];
